cf05375c0ee28c19bbbd70cf574e50080b3b689f
dannel <dengyuanlu@gmail.com>
2018-05-24 17:26:17 +1200

    Version 2
    this version can only connect to database and reply

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -33,18 +33,18 @@ namespace Bot_Application2.Dialogs
             switch(optionSelected)
             {
                 case "ShowAllMajors":
-                    await context.PostAsync("That this dialog is not implemented yet");
+                    await context.PostAsync("That this dialog is not implemented yet, typing anything to show the selection again!");
                     context.Wait(MessageReceivedAsync);
                     break;
                 case "SearchByName":
-                    //context.Call(new MajorSearch(DSA), ResumeAfterOptionDialog);
+                    context.Call(new MajorSearch(), ResumeAfterOptionDialog);
                     break;
             }
         }
 
-       /* private async Task ResumeAfterOptionDialog(IDialogContext context, IAwaitable<object> result)
+       private async Task ResumeAfterOptionDialog(IDialogContext context, IAwaitable<object> result)
         {
             context.Wait(MessageReceivedAsync);
-        }*/
+        }
     }
 }
